50ab47884f2c3fac92ddecd7e9d823501f48faa1
barisgonul16 <bariss.gonul16@gmail.com>
2025-11-23 10:49:17 +0300

    feat: backend test button

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // ← BURAYI DEĞİŞTİR! Render'dan aldığın tam URL
-const API_BASE = 'https://productivity-dashboard-1.onrender.com'; // örnek
+const API_BASE = 'https://productivity-dashboard-q9xr.onrender.com'; // örnek
 
 export const api = axios.create({
   baseURL: API_BASE,
